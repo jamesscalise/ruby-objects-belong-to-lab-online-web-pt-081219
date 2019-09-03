d65695808dd92ff05bfcbf625547e763510e766a
James Scalise <jamescscalise@gmail.com>
2019-09-03 20:43:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :title, :artist
-  def initialize(name = "", artist = nil)
-    @title = name
-    @artist = artist
+ # def initialize(name = "", artist = nil)
+ #   @title = name
+  #  @artist = artist
   end
 end
